f6db1c460ef52d2e43b924fb5ebb12a24d3e3912
Akond Rahman <akond.rahman.buet@gmail.com>
2016-12-08 10:10:13 -0500

    extra code metric:favorite

--- src/favre.R
+++ src/favre.R
@@ -22,6 +22,33 @@ names(dic_topics) <- doc_names
 ###threshold 
 topic_prob_cutoff <- 0.10
 
+for(doc_ind in 2:len_doc_names)
+{
+  
+  topics_for_this_doc <- vector(mode="numeric", length=len_top_names+1)
+  dom_topic_for_this_doc <- rep(0, len_top_names+1)
+  for(topic_ind in 1:len_top_names+1)
+  {
+    topic_prob_for_doc <- topic_prob_data[doc_ind, topic_ind]
+    topics_for_this_doc[topic_ind] <- topic_prob_for_doc
+  }
+  #max_topic_prob <-  max(topics_for_this_doc)
+  #print(max_topic_prob)
+  index_with_max_prob <- which(topics_for_this_doc >= topic_prob_cutoff)
+  #print("-----")
+  #print(index_with_max_prob)
+  dom_topic_for_this_doc[index_with_max_prob] <- 1
+  #print(dom_topic_for_this_doc)
+  dic_topics[[doc_ind]] <- dom_topic_for_this_doc
+  #print("-----")  
+}
+len_dic_topic   <-  length(dic_topics)        ## get the count of all questions in the corpus 
+fav_count_vector    <-  content_data$FavoriteCount   ## get favorite count from content file 
+fav_count_vector    <-  as.numeric(fav_count_vector)    ## convert to number 
+print("------------Summary of favorite------------------")
+print(summary(fav_count_vector))
+
+
 t2 <- Sys.time()
 print(t2 - t1)  
 rm(list = setdiff(ls(), lsf.str()))
