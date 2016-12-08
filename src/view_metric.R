f138ec666acbc9f6cc21dbdc3fa8e0dcdf01c708
Akond Rahman <akond.rahman.buet@gmail.com>
2016-12-07 21:41:22 -0500

    extra metric: view

--- src/view_metric.R
+++ src/view_metric.R
@@ -9,9 +9,81 @@ content_data <- read.csv(content_file)
 topic_prob_file <- "/Users/akond/Documents/AkondOneDrive/OneDrive/StackOverflowProject/DevOpsInSO/output/rq1/with_title_all_corpus_20_topics/_TopicProb.csv"
 topic_prob_data <- read.csv(topic_prob_file)
 
+topic_names <- colnames(topic_prob_data, do.NULL = TRUE, prefix = "col")
+doc_names <- rownames(topic_prob_data, do.NULL = TRUE, prefix = "row")
 
-
-
+topic_names <- topic_names[!topic_names %in% "X"]
+#print(topic_names)
+#print(doc_names)
+len_doc_names <- length(doc_names)
+len_top_names <- length(topic_names)
+dic_topics <- vector(mode="list", length=len_doc_names)
+names(dic_topics) <- doc_names
+###threshold 
+topic_prob_cutoff <- 0.10
+for(doc_ind in 2:len_doc_names)
+{
+  
+  topics_for_this_doc <- vector(mode="numeric", length=len_top_names+1)
+  dom_topic_for_this_doc <- rep(0, len_top_names+1)
+  for(topic_ind in 1:len_top_names+1)
+  {
+    topic_prob_for_doc <- topic_prob_data[doc_ind, topic_ind]
+    topics_for_this_doc[topic_ind] <- topic_prob_for_doc
+  }
+  #max_topic_prob <-  max(topics_for_this_doc)
+  #print(max_topic_prob)
+  index_with_max_prob <- which(topics_for_this_doc >= topic_prob_cutoff)
+  #print("-----")
+  #print(index_with_max_prob)
+  dom_topic_for_this_doc[index_with_max_prob] <- 1
+  #print(dom_topic_for_this_doc)
+  dic_topics[[doc_ind]] <- dom_topic_for_this_doc
+  #print("-----")  
+}
+len_dic_topic <- length(dic_topics)        ## get the count of all questions in the corpus 
+viewvector    <-  content_data$ViewCount   ## get view counts from content file 
+viewvector    <- as.numeric(viewvector)    ## convert to number 
+print("------------Summary of view count------------------")
+print(summary(viewvector))
+for(top_inex in 1:len_top_names+1)
+{
+  ### temp view count vector 
+  temp_view_count_vector <- vector(mode="numeric", length=len_dic_topic)
+  ### counter_ for all questions 
+  counter_ <- 0 
+  ### counter for questions for this topic 
+  q_count_topic <- 0 
+  print("#########################")
+  print("Topic #")
+  print(top_inex-1)
+  for(doc_index in 2:len_dic_topic)
+  {
+    counter_ <- counter_ + 1 
+    tmp_ <-  dic_topics[[doc_index]][top_inex]
+    if(tmp_==1)
+    {
+      q_count_topic <- q_count_topic + 1
+      viewForQues <- viewvector[counter_]
+      #print(viewForQues)
+      #print("===============")
+      temp_view_count_vector <- c( temp_view_count_vector,  viewForQues)
+    }
+  }
+  
+  print("***Total questions in this topic***")
+  print(q_count_topic)
+  print("***Views for questions***")
+  temp_view_count_vector <- unique(temp_view_count_vector[temp_view_count_vector != ""])
+  temp_view_count_vector <- unique(temp_view_count_vector[temp_view_count_vector !=0])  
+  temp_view_count_vector <- temp_view_count_vector[!is.na(temp_view_count_vector)] 
+  view_for_topic <- sum(temp_view_count_vector)
+  print(view_for_topic)  
+  view_per_q     <- (view_for_topic / q_count_topic ) 
+  print("===Views per question===")
+  print(view_per_q)
+  print("#########################")
+}
 t2 <- Sys.time()
-print(t2 - t1)  # 
+print(t2 - t1)  
 rm(list = setdiff(ls(), lsf.str()))
