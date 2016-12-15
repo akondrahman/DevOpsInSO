62dfbb6819be677aeebadbfc096a25b4f5e6038b
Akond Rahman <akond.rahman.buet@gmail.com>
2016-12-15 11:51:52 -0500

    trend topic

--- src/trend_topic_anal.py
+++ src/trend_topic_anal.py
@@ -7,16 +7,29 @@ import csv
 def formatQues(dateStr):
    dstr=''
    splitted_  = dateStr.split('/')
-   month_     = splitted_[0]
-   year_      = splitted_[-1]
+   tmp_month_     = splitted_[0]
+   tmp_year_      = splitted_[-1]
+   if len(tmp_year_) == 2:
+      year_ = '20' + tmp_year_
+   elif len(tmp_year_) >= 4:
+      year_splitted = tmp_year_.split('-')
+      year_  = year_splitted[0]
+   if len(tmp_month_) >= 4:
+      month_splitted = tmp_month_.split('-')
+      month_  = month_splitted[1]
+   else:
+      month_  = tmp_month_
+   if len(month_)==1:
+     month_ = '0' + month_
    dstr       = year_ + '-' + month_
-   return dstr       
+   print dstr
+   return dstr
 
 def getDateWiseDict(fileNameParam):
   dateWiseDict={}
   with open(fileNameParam, 'rU') as f:
     reader = csv.reader(f)
-    next()
+    next(reader)
     for row in reader:
       idOfQues      = int(row[0])
       date_         = row[1]
@@ -29,9 +42,9 @@ def getDateWiseDict(fileNameParam):
       else:
         tmp_ = dateWiseDict[dateOfQues]
         dateWiseDict[dateOfQues]  = tmp_ + [tupToInsert]
-
+  return dateWiseDict
 
 
 f_ = '/Users/akond/Documents/AkondOneDrive/OneDrive/StackOverflowProject/DevOpsInSO/output/rq2/RQ2_QAA_TrendInp.csv'
 dateDict=getDateWiseDict(f_)
-print dateDict
+#print dateDict
