43745aade34e625ff5f5ef175471fc5207247b89
Akond Rahman <akond.rahman.buet@gmail.com>
2017-03-25 17:00:02 -0400

    esem stuff

--- src/esem_rq5_prep.py
+++ src/esem_rq5_prep.py
@@ -22,7 +22,26 @@ def loadDatasetByDate(path_to_file):
 
 
 
+def getDatesOfChallenges(datasetFile):
+  rq5Dict={}
+  with open(path_to_file, 'rU') as f:
+     reader_ = csv.reader(f)
+     next(reader_)
+     for row_ in reader_:
+       date_month        = row_[2]
+       challenge         = row_[5]
+       if challenge not in rq5Dict:
+           rq5Dict[challenge] = [challenge]
+       else:
+           rq5Dict[challenge] =  rq5Dict[challenge] + [challenge]
+  return rq5Dict
+
+
 datasetFile='/Users/akond/Documents/AkondOneDrive/OneDrive/StackOverflowProject/data/ESEM/COMPLETE_DATASET_FOR_PAPER.csv'
 rq5Dict = loadDatasetByDate(datasetFile)
-print "We have data of {} months".format(rq5Dict)
+print "We have data of {} months ... ".format(len(rq5Dict))
 print "="*100
+challenge_date_dict = getDatesOfChallenges(datasetFile)
+print "We are looking at {} challenges ...".format(len(challenge_date_dict))
+print "="*100
+getChallengeWiseTemporalValues(rq5Dict, challenge_date_dict)
