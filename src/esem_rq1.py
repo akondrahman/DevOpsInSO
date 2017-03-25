6bc473803e6963a4d71e08b14ec6c225c5ae5759
Akond Rahman <akond.rahman.buet@gmail.com>
2017-03-25 00:40:40 -0400

    esem stuff

--- src/esem_rq1.py
+++ src/esem_rq1.py
@@ -10,7 +10,7 @@ import csv, os, numpy as np, utility
 def loadDatasetByChallenge(path_to_file):
   rq1Dict={}
   totalLineCount = 0
-  with open(file_, 'rU') as f:
+  with open(path_to_file, 'rU') as f:
      reader_ = csv.reader(f)
      next(reader_)
      for row_ in reader_:
@@ -20,5 +20,23 @@ def loadDatasetByChallenge(path_to_file):
        if challenge not in rq1Dict:
            rq1Dict[challenge] = [qID]
        else:
-           rq1Dict[challenge] =  rq1Dict[challenge] + [qID]            
+           rq1Dict[challenge] =  rq1Dict[challenge] + [qID]
   return rq1Dict, totalLineCount
+
+
+
+
+
+def provideAnswerToRQ1(dict_, count_):
+    for challenge_, qs in dict_.items():
+        perc_ = round((len(qs))/(float(count_)), 4)
+        perc_ = perc_ * 100
+        print "Challenge:{}, percenatge:{}".format(challenge_, perc_)
+        print "*"*50
+
+
+datasetFile='/Users/akond/Documents/AkondOneDrive/OneDrive/StackOverflowProject/data/ESEM/COMPLETE_DATASET_FOR_PAPER.csv'
+rq1Dict, countOfQs = loadDatasetByChallenge(datasetFile)
+print "We will be analyzing {} questions".format(countOfQs)
+print "="*100
+provideAnswerToRQ1(rq1Dict, countOfQs)
