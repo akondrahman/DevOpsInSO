45e6f69fcdd85a0253375b78f15e53c8b059891b
Akond Rahman <akond.rahman.buet@gmail.com>
2017-03-25 12:29:03 -0400

    esem stuff

--- src/esem_rq4.py
+++ src/esem_rq4.py
@@ -20,3 +20,28 @@ def loadDatasetByChallenge(path_to_file):
        else:
            rq1Dict[challenge] =  rq1Dict[challenge] + [qID]
   return rq1Dict
+
+
+def loadDatasetByProgrammerCount(path_to_file):
+  rq4Dict={}
+  totalLineCount = 0
+  with open(path_to_file, 'rU') as f:
+     reader_ = csv.reader(f)
+     next(reader_)
+     for row_ in reader_:
+       totalLineCount = totalLineCount + 1
+       qID              = row_[0]
+       challenge        = row_[5]
+       if challenge not in rq4Dict:
+           rq4Dict[challenge] = [qID]
+       else:
+           rq4Dict[challenge] =  rq4Dict[challenge] + [qID]
+  return rq4Dict, totalLineCount
+
+
+
+
+
+datasetFile='/Users/akond/Documents/AkondOneDrive/OneDrive/StackOverflowProject/data/ESEM/COMPLETE_DATASET_FOR_PAPER.csv'
+ques_challenge_dict = loadDatasetByChallenge(datasetFile)
+print "="*100
