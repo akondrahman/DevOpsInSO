04ef65a3cbeda41ddebe3a2cbf4515c53a6b8c29
Akond Rahman <akond.rahman.buet@gmail.com>
2017-03-25 12:40:05 -0400

    esem stuff

--- src/esem_rq4.py
+++ src/esem_rq4.py
@@ -30,15 +30,25 @@ def loadDatasetByProgrammerCount(path_to_file):
      next(reader_)
      for row_ in reader_:
        totalLineCount = totalLineCount + 1
-       qID              = row_[0]
+       programmerID     = row_[4]
        challenge        = row_[5]
        if challenge not in rq4Dict:
-           rq4Dict[challenge] = [qID]
+           rq4Dict[challenge] = [programmerID]
        else:
-           rq4Dict[challenge] =  rq4Dict[challenge] + [qID]
+           rq4Dict[challenge] =  rq4Dict[challenge] + [programmerID]
   return rq4Dict, totalLineCount
 
 
+
+
+
+def provideAnswerToRQ4(p_dict_, q_dict):
+    for challenge_, qs in q_dict.items():
+        q_count = len(qs)
+        p_count = len(np.unique(p_dict_[challenge_]))
+        print "Challenge: {}, programmer count:{}, ques count:{}".format(challenge_, p_count, q_count)
+        #print "Challenge:{}, percenatge:{}".format(challenge_, perc_)
+        print "*"*50
 
 
 
@@ -48,4 +58,6 @@ print "Loaded the questions for each challenge"
 print "="*100
 programmer_dict, qcount =loadDatasetByProgrammerCount(datasetFile)
 print "We will be analyzing {} questions".format(qcount)
+print "="*100
+provideAnswerToRQ4(programmer_dict, ques_challenge_dict)
 print "="*100
